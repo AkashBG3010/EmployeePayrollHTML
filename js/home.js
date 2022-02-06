6abb1059ed89748bd92132831013866e85c94266
Akash B G <akashbpura@gmail.com>
2022-02-07 00:15:10 +0530

    Updated to display all employee by json object

--- js/home.js
+++ js/home.js
@@ -11,21 +11,57 @@ const createInnerHtml = () => {
       <th>Salary</th>
       <th>Start Date</th>
       <th>Actions</th>`;
-    const innerHtml = `${headerHtml}
+    
+    let innerHtml = `${headerHtml}`;
+    let employeePayrollData = createEmployeePayrollJASON();
+    for (const employeePayrollData of employeePayrollList) {
+        innerHtml = `$${headerHtml}
     <tr>
-    <td><img class="profile" src="../assets/profile-images/Ellipse -4.png" alt=""></td>
-    <td>Niharika Deph</td>
-    <td>Female</td>
+    <td><img class="profile" src="${employeePayrollData._profilePic}" alt=""></td>
+    <td>${employeePayrollData._name}</td>
+    <td>${employeePayrollData._gender}</td>
+    <td>${getDeptHtml(employeePayrollData._department)}</td>
+    <td>${employeePayrollData._salary}</td>
+    <td>${employeePayrollData._startDate}</td>
+    _
     <td>
-      <div class="dept-label">HR</div>
-      <div class="dept-label">Finance</div>
-    </td>
-    <td>3000000</td>
-    <td>22 Jan 2020</td>
-    <td>
-      <img src="../assets/icons/delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
-      <img src="../assets/icons/create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
+      <img src="../assets/icons/delete-black-18dp.svg" alt="delete" image name="${employeePayrollData._id}" onclick="remove(this)">
+      <img src="../assets/icons/create-black-18dp.svg" alt="edit" image name="${employeePayrollData._id}" onclick="update(this)">
     </td>
   </tr>`;
 document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptList = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml}<div class= 'dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
+const createEmployeePayrollJASON = () => {
+    let employeePayrollListData = [
+        {
+            _name: 'Akash BG',
+            _gender: 'Male',
+            _department: ['Engineering', 'Finance'],
+        _salary: '500000',
+        _startDate: '16 jun 2021',
+        _note: '',
+        _id: new Date().getTime(),
+        _profilePic: '../assets/profile-images/Ellipse -2.png'
+        },
+        {
+        _name: 'Sameep Pai',
+        _gender: 'Male',
+        _department: ['Sales'],
+        _salary: '500000',
+        _startDate: '15 Dec 2020',
+        _note: '',
+        _id: new Date().getTime() +1,
+        _profilePic: '../assets/profile-images/Ellipse -4.png'
+        }
+    ];
+    return employeePayrollListData;
 }
